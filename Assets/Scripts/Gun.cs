8693fa2a54d35c116ce8145c0dbb0f9752fc8423
unknown <stavbi1@gmail.com>
2022-08-17 23:37:39 +0300

    gun

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float range = 100f;
+
+    public Camera fpsCamera;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
